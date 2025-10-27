901f2a4f58985d647a5a0508f4cc37f7768c9d16
Omer Geva <54701359+OmerGeva@users.noreply.github.com>
2025-10-27 21:06:17 +0200

    feat: klaviyo script (#23)

--- components/layout/third-party-scripts.component.tsx
+++ components/layout/third-party-scripts.component.tsx
@@ -31,6 +31,10 @@ const ThirdPartyScripts = () => {
 
       {allowMarketing && (
         <>
+         <Script
+            src={`https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=WBAM5n`}
+            strategy="afterInteractive"
+          />
           <Script src="https://cdn.attn.tv/semiaquatics/dtag.js" strategy="afterInteractive" />
           <Script id="meta-pixel" strategy="afterInteractive">
             {`
